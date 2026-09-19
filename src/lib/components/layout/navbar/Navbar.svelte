53502f52bf2d5bf93a95115ef687c913b98995db
Repen Valinta <repen.valinta@proton.me>
2026-09-19 18:09:27 +0300

    homepage part 1 done

---
+++ src/lib/components/layout/navbar/Navbar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import DesktopNav from './DesktopNav.svelte';
+</script>
+
+<nav>
+	<DesktopNav />
+</nav>
+
+<style>
+	nav {
+		position: fixed;
+		inset: 0;
+		z-index: 10;
+		font-weight: 500;
+		letter-spacing: 0.05em;
+	}
+</style>
